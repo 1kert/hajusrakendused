06481fa64e9cc526b3ed7a10b513058dac609e88
1kert <kertmand1@gmail.com>
2024-12-18 08:22:37 +0200

    jwt middleware

--- Program.cs
+++ Program.cs
@@ -1,7 +1,25 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options => 
+    {
+        options.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = "dunno",
+            ValidAudience = "dunno",
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(""))
+        };
+    });
 
 var app = builder.Build();
 
@@ -15,6 +33,8 @@ if (!app.Environment.IsDevelopment())
 
 // app.UseHttpsRedirection();
 app.UseRouting();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 

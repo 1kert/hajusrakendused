cde3cf4aade4becad8d0a30e0c28c1ec503f1663
Kert Mänd <kertmand1@gmail.com>
2025-05-02 09:14:19 +0300

    title editing

--- clientApp/src/components/MarkerDialog.tsx
+++ clientApp/src/components/MarkerDialog.tsx
@@ -1,6 +1,8 @@
-import {Dialog, DialogContent, DialogHeader} from "./ui/dialog.tsx";
+import {Dialog, DialogContent, DialogFooter, DialogHeader} from "./ui/dialog.tsx";
 import ic_edit from "../assets/ic_edit.svg"
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {Input} from "./ui/input.tsx";
+import { Button } from "./ui/button.tsx";
 
 export default function MarkerDialog(
     props: {
@@ -10,23 +12,49 @@ export default function MarkerDialog(
         author: string
         updateDate: string
         canEdit: boolean
+        onEdit: (title: string, description: string) => void
         onClose: () => void
     }
 ) {
-    const [isEditing, setIsEditing] = useState<boolean>(false)
+    const [isEditing, setIsEditing] = useState(false)
+    const [title, setTitle] = useState("")
+    const [description, setDescription] = useState("")
+    
+    useEffect(() => { // todo: remove when finished
+        handleOnEditClick()
+    }, [])
+    
+    function handleOnEditClick() {
+        setTitle(props.title)
+        setDescription(props.description)
+        setIsEditing(true)
+    }
+    
+    function handleOnConfirmClick() {
+        setIsEditing(false)
+        props.onEdit(title, description)
+        // todo: handle loading
+    }
     
     return (
         <Dialog open={props.isVisible} onOpenChange={open => { if(!open) props.onClose() }}>
             <DialogContent>
                 <DialogHeader>
                     <div className="flex gap-2 pr-4">
-                        <h1 className="text-xl">{props.title}</h1>
-                        { props.canEdit && <img src={ic_edit} alt="edit" className="size-6 relative top-0.5 hover:cursor-pointer" /> }
+                        {/*<h1 className="text-xl">{props.title}</h1>*/}
+                        <Input type="text" onChange={e => setTitle(e.target.value)} value={title}/>
+                        { props.canEdit && <img src={ic_edit} alt="edit" onClick={handleOnEditClick} className="size-6 relative top-0.5 hover:cursor-pointer" /> }
                     </div>
                     <p className="text-xs text-gray-700">Created by {props.author}</p>
                     <p className="text-xs text-gray-500">{props.updateDate}</p>
                 </DialogHeader>
                 <p>{props.description}</p>
+                { isEditing && (
+                    <DialogFooter>
+                        <Button variant="outline" onClick={() => setIsEditing(false)}>Cancel</Button>
+                        <Button onClick={handleOnConfirmClick}>Confirm</Button>
+                    </DialogFooter>
+                )}
             </DialogContent>
         </Dialog>
     )

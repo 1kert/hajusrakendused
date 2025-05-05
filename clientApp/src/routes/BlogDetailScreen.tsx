584a73907f7c2415164650232ab757c4093f722a
Kert Mänd <kertmand1@gmail.com>
2025-05-06 00:54:58 +0300

    deleting functionality

--- clientApp/src/routes/BlogDetailScreen.tsx
+++ clientApp/src/routes/BlogDetailScreen.tsx
@@ -1,6 +1,13 @@
 import {useNavigate, useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import BlogRepository, {Blog} from "../repositories/BlogRepository.tsx";
+import vertical_menu from "../assets/ic_vertical_menu.svg"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger
+} from "../components/ui/dropdown-menu.tsx";
 
 export default function BlogDetailScreen() {
     const params = useParams()
@@ -11,10 +18,24 @@ export default function BlogDetailScreen() {
         (async () => {
             const id = Number(params.id)
             if (!Number.isInteger(id)) navigate("/blogs")
-            const blog = await BlogRepository.get(id)
-            setBlog(blog)
+            await getBlog(id)
         })()
-    })
+    }, [])
+    
+    async function getBlog(id: number | undefined = blog?.id) {
+        if (!id) throw Error("Blog not found")
+        setBlog(await BlogRepository.get(id))
+    }
+    
+    async function onCommentEdit(id: number, content: string) {
+        await BlogRepository.editComment(id, content)
+        await getBlog()
+    }
+    
+    async function onCommentDelete(id: number) {
+        await BlogRepository.deleteComment(id)
+        await getBlog()
+    }
     
     // todo: loading
     // todo: error
@@ -29,7 +50,14 @@ export default function BlogDetailScreen() {
                     <p className="text-xl mt-10">Comments</p>
                     <div className="mt-5 gap flex flex-col gap-4">
                         {blog.comments?.map(comment => 
-                            <CommentCard content={comment.content} author={comment.author} updatedAt={comment.updatedAt} />
+                            <CommentCard
+                                key={comment.id}
+                                content={comment.content}
+                                author={comment.author}
+                                updatedAt={comment.updatedAt}
+                                onEdit={comment.canEdit ? (content: string) => onCommentEdit(comment.id, content) : undefined }
+                                onDelete={comment.canDelete ? () => onCommentDelete(comment.id) : undefined }                                
+                            />
                         )}
                     </div>
                 </div>
@@ -43,12 +71,33 @@ function CommentCard(
         content: string
         author: string
         updatedAt: string
+        onEdit?: (content: string) => void
+        onDelete?: () => void
     }
 ) {
+    function onEditClick() {
+        
+    }
+    
     return (
         <div className="flex flex-col gap-1 bg-gray-300 px-3 py-2 rounded-md shadow-md">
-            <p className="font-bold">{props.author}</p>
-            <p className="text-sm text-gray-600">{props.updatedAt}</p>
+            <div className="flex justify-between">
+                <div className="flex flex-col gap-1">
+                    <p className="font-bold">{props.author}</p>
+                    <p className="text-sm text-gray-600">{props.updatedAt}</p>
+                </div>
+                {(props.onEdit || props.onDelete) &&
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <img className="size-7 mt-1 hover:cursor-pointer" src={vertical_menu} alt="more"/>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            {props.onEdit && <DropdownMenuItem onClick={onEditClick}>Edit</DropdownMenuItem>}
+                            {props.onDelete && <DropdownMenuItem onClick={props.onDelete} className="text-red-600">Delete</DropdownMenuItem>}
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                }
+            </div>
             <p className="mt-2">{props.content}</p>
         </div>
     )

2840829e731a0c41b06fda803f44af579dd08593
Kert Mänd <kertmand1@gmail.com>
2025-05-06 20:20:07 +0300

    local store items

--- clientApp/src/routes/store/StoreScreen.tsx
+++ clientApp/src/routes/store/StoreScreen.tsx
@@ -1,7 +1,35 @@
+import {useEffect, useState} from "react";
+import StoreRepository, {StoreItem} from "../../repositories/StoreRepository.ts";
+
 export default function StoreScreen() {
+    const [storeItems, setStoreItems] = useState<StoreItem[]>([])
+    
+    useEffect(() => {
+        (async () => {
+            const items = await StoreRepository.getStoreItems()
+            setStoreItems(items)
+        })()
+    }, [])
+    
     return (
         <div className="flex flex-col w-full">
-            <h1>Store screen</h1>
+            <div className="flex flex-col gap-2">
+                {storeItems.map(item => (
+                    <StoreItemCard name={item.name} description={item.description} image={item.image} />
+                ))}
+            </div>
         </div>
+    )
+}
+
+function StoreItemCard(
+    props: {
+        name: string
+        description: string
+        image: string
+    }
+) {
+    return (
+        <img src={props.image} alt="" className="size-52"/>
     )
 }
